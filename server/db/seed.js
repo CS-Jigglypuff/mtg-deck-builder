9b02717be7182628b9b31240b50a4737e66bcedb
Nate Toll <nate.toll@gmail.com>
2023-04-25 15:49:14 -0400

    normalized data destructrued from set

--- server/db/seed.js
+++ server/db/seed.js
@@ -27,7 +27,32 @@ async function fetchData() {
         if (mana_cost === '') {
           mana_cost = '0';
         }
-        console.log(mana_cost);
+        if (oracle_text === undefined) {
+          oracle_text = card_faces[0].oracle_text;
+        }
+        if (power === undefined && type_line.includes('Creature')) {
+          power = card_faces[0].power;
+        }
+        if (toughness === undefined && type_line.includes('Creature')) {
+          toughness = card_faces[0].toughness;
+        }
+        if (image_uris === undefined) {
+          image_uris = card_faces[0].image_uris;
+        }
+        image_uris = image_uris.normal;
+        color_identity = color_identity.toString();
+        console.log({
+          id,
+          name,
+          rarity,
+          type_line,
+          color_identity,
+          mana_cost,
+          oracle_text,
+          power,
+          toughness,
+          image_uris,
+        });
       })
     );
 }

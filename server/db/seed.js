e3a26bf7133a3e44b854a457e0ceb69444d8a654
Nate Toll <nate.toll@gmail.com>
2023-04-25 15:31:47 -0400

    normalizing mana_cost from all cards fetch request

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,7 +1,35 @@
 // run this file to seed the db
 
-fetch('https://api.scryfall.com/cards/search?q=set:khm', {
-  method: 'get',
-})
-  .then((response) => response.json())
-  .then((data) => console.log(data.data[0].name));
+async function fetchData() {
+  await fetch('https://api.scryfall.com/cards/search?q=set:khm', {
+    method: 'get',
+  })
+    .then((response) => response.json())
+    .then((cards) => cards.data)
+    .then((cardArray) =>
+      cardArray.forEach((card) => {
+        let {
+          id,
+          name,
+          rarity,
+          type_line,
+          color_identity,
+          mana_cost,
+          oracle_text,
+          power,
+          toughness,
+          image_uris,
+          card_faces,
+        } = card;
+        if (mana_cost === undefined) {
+          mana_cost = card_faces[0].mana_cost;
+        }
+        if (mana_cost === '') {
+          mana_cost = '0';
+        }
+        console.log(mana_cost);
+      })
+    );
+}
+
+fetchData();

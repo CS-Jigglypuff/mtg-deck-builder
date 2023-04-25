1c568495c8691b3f37ffba785e009d5c69d79a3e
Yomkool <bannon.pj@gmail.com>
2023-04-25 11:32:11 -0400

    Add hot module reload

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
-import path from 'path'
-import express from 'express'
-
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
 
 app.use(express.static(__dirname));
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,33 @@
-const path = require("path")
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  entry: {
-    main: './src/index.js'
-  },
+  entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/',
-    filename: '[name].js'
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "[name].js",
   },
-  target: 'web',
-  devtool: 'source-map',
+  mode: "development",
+  devServer: {
+    host: "localhost",
+    port: "8080",
+    hot: true,
+    open: true,
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+      publicPath: "/",
+    },
+    liveReload: true,
+    proxy: {
+      "/": {
+        target: "http://localhost:3000/",
+      },
+    },
+  },
+  target: "web",
+  devtool: "source-map",
   module: {
     rules: [
       {
@@ -20,34 +36,35 @@ module.exports = {
         loader: "babel-loader",
       },
       {
-        // Loads the javacript into html template provided.
-        // Entry point is set below in HtmlWebPackPlugin in Plugins 
         test: /\.html$/,
         use: [
           {
             loader: "html-loader",
-            //options: { minimize: true }
-          }
-        ]
+          },
+        ],
       },
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        use: ["style-loader", "css-loader"],
       },
       {
-       test: /\.(png|svg|jpg|gif)$/,
-       use: ['file-loader']
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
       },
-    ]
+    ],
   },
   resolve: {
-    extensions: [".js", ".jsx"]
+    extensions: [".js", ".jsx"],
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/html/index.html",
       filename: "./index.html",
-      excludeChunks: [ 'server' ]
-    })
-  ]
-}
+      excludeChunks: ["server"],
+    }),
+    new webpack.ProvidePlugin({
+      React: "react",
+    }),
+    new webpack.HotModuleReplacementPlugin(), // Add this line
+  ],
+};
